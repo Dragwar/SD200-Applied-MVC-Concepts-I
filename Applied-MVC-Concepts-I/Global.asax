83c9b72f89270096efe73913a724139bda452681
Everett Grassler <dragwar9@gmail.com>
2019-03-12 10:09:18 -0500

    Add project files.

---
+++ Applied-MVC-Concepts-I/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Applied_MVC_Concepts_I.MvcApplication" Language="C#" %>
---
+++ Applied-MVC-Concepts-I/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Applied_MVC_Concepts_I.Startup))]
+namespace Applied_MVC_Concepts_I
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
